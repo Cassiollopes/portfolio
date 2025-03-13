a11cf8440d656a93501762a0025bc8bab2c76702
cassio <cassio8976@gmail.com>
2025-03-13 11:14:43 -0300

    modified:   src/components/nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,7 +6,8 @@ import { BsMoonStars, BsSun } from "react-icons/bs";
 import { useTheme } from "next-themes";
 
 export default function Nav() {
-  const { setTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === "dark";
 
   return (
     <nav className="w-full p-4 sticky top-0 z-50 border-b backdrop-blur-xl ">
@@ -25,9 +26,7 @@ export default function Nav() {
           ))}
           <button
             className="pl-4 cursor-pointer opacity-50 dark:opacity-80 dark:hover:opacity-100 hover:opacity-100 transition-all duration-200 ease-in-out"
-            onClick={() =>
-              setTheme((theme) => (theme === "dark" ? "light" : "dark"))
-            }
+            onClick={() => setTheme(isDarkMode ? "light" : "dark")}
           >
             <BsSun className="hidden dark:block" />
             <BsMoonStars className="dark:hidden" />
