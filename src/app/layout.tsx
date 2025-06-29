44572bedc5ea158b497928e4a99f1f698de21ae3
cassio <cassio8976@gmail.com>
2025-06-29 16:57:06 -0300

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br" suppressHydrationWarning>
       <body
-        className={`${inter.className} ${geistSans.variable} antialiased scroll-smooth bg-primary-foreground  px-5 pt-20 max-md:pt-8 pb-32 select-none dark:bg-black/40`}
+        className={`${inter.className} ${geistSans.variable} antialiased scroll-smooth bg-primary-foreground  px-5 pt-20 max-md:pt-8 pb-32 select-none dark:bg-black/20`}
       >
         <ThemeProvider
           attribute="class"
