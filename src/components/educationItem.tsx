b43a4428b0ebbe125c3387b9907eb71a168a457e
cassio <cassio8976@gmail.com>
2025-07-02 16:06:54 -0300

    modified:   src/components/educationItem.tsx

--- src/components/educationItem.tsx
+++ src/components/educationItem.tsx
@@ -37,7 +37,7 @@ export default function EducationItem({
             </h3>
             <p className="text-sm whitespace-nowrap">{date}</p>
           </div>
-          <div className="truncate leading-none text-xs text-foreground">{description}</div>
+          <div className="truncate leading-none text-xs">{description}</div>
         </div>
       </div>
     </Link>
