cbdaf803894f0d2a653869639ada46786bb952b0
cassio <cassio8976@gmail.com>
2025-07-07 08:18:57 -0300

    modified:   src/components/nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -29,7 +29,7 @@ export default function Nav() {
 
   return (
     <nav className="w-full flex justify-center items-center fixed bottom-0 left-0 right-0 z-50 bg-gradient-to-b from-transparent to-black/[0.02] dark:to-white/[0.005] py-10 pb-5">
-      <div className="flex gap-6 items-center text-xl border rounded-full bg-background p-4 px-6 shadow-sm animate-scaleUp">
+      <div className="flex items-center text-xl border rounded-full bg-background p-4 shadow-sm animate-scaleUp cursor-pointer">
         {links.map((link, i) => (
           <Link
             key={i}
@@ -40,18 +40,22 @@ export default function Nav() {
                 ? "curriculo-cassio-eduardo.pdf"
                 : undefined
             }
-            className="cursor-pointer transition-all duration-200 ease-in-out group relative flex items-center justify-center"
+            className="cursor-pointer transition-all duration-200 ease-in-out group relative flex items-center justify-center hover:mx-4 w-[40px] h-[20px]"
           >
-            {link.icon}
-            <div className="absolute rounded-full dark:bg-white/10 bg-black/10 opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100 p-5" />
-            <div className="absolute rounded-full p-1 px-3 -top-12 opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100 invisible group-hover:visible bg-black text-white dark:bg-white dark:text-black text-sm">
+            <div className="group-hover:text-3xl transition-all duration-200 ease-linear absolute group-hover:mb-4">
+              {link.icon}
+            </div>
+            <div className="rounded-full dark:bg-white/10 bg-black/10 opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100 p-5" />
+            <div className="absolute rounded-full p-1 px-3 -top-12 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 invisible group-hover:visible bg-black text-white dark:bg-white dark:text-black text-sm">
               {link.name}
             </div>
           </Link>
         ))}
-        <div className="border-l h-[20px] w-[1px]"></div>
+        <div className="w-[20px] justify-center items-center flex">
+          <div className="border-l h-[20px] w-[1px]"></div>
+        </div>
         <button
-          className="cursor-pointer opacity-50 hover:opacity-100 transition-all duration-200 ease-in-out"
+          className="cursor-pointer opacity-50 hover:opacity-100 transition-all duration-200 ease-in-out w-[40px] flex items-center justify-center"
           onClick={() => setTheme(isDarkMode ? "light" : "dark")}
         >
           <BsSun className="hidden dark:block" />
